908c3c81e550585da334743fb199b6394aff6f91
Ryo-N7 <ryonakagawara@gmail.com>
2021-09-18 17:00:28 +0900

    change to linux. write up more instructions. cut down on dependencies.

--- scripts/Get-GoogleDrive-Data.R
+++ scripts/Get-GoogleDrive-Data.R
@@ -1,27 +1,33 @@
 ## Load packages
-library(dplyr)
 library(googledrive)
 library(purrr)
-library(stringr)
-library(fs)
+#library(dplyr)
+#library(stringr)
+#library(fs)
 options(googledrive_quiet = TRUE)
 
 ## Authenticate into googledrive service account
+## GCRED.JSON is the JSON file filled with our Google credentials that we saved inside the GHA run in the step prior to running the R script.
+## Note that we never have a GCRED.JSON file within our own repository because we never commit that file at the end of the GHA run.
+#googledrive::drive_auth(path = "~/.R/gargle/indigo-bazaar-325705-4c1134b651dc.json")
 googledrive::drive_auth(path = "GCRED.JSON")
 
 ## Find Google Drive folder 'Centre Circle Data & Info'
 data_folder <- drive_ls(path = "Centre Circle Data & Info")
 
 ## Filter for just the .csv files
-data_csv <- data_folder %>% filter(str_detect(name, ".csv")) %>% arrange(name)
+data_csv <- data_folder[grepl(".csv", data_folder$name), ]
+#data_csv <- data_folder %>% filter(str_detect(name, ".csv")) %>% arrange(name)
 
 ## download function
-get_Drive_CPL_data <- function(g_id, data_name) {
-  cat("... Trying to download", data_name, "...")
+get_drive_cpl_data <- function(g_id, data_name) {
+  cat("\n... Trying to download", data_name, "...\n")
   
   ## Set folder for downloads
   data_path <- "data"
-  fs::dir_create(data_path)
+  dir.create(data_path) 
+  ## dir.create will fail if folder already exists so not great for scripts on local but as GHA is creating a new environment every time it runs we won't have that problem here
+  #fs::dir_create(data_path)
   
   # Wrap drive_download function in safely()
   safe_drive_download <- purrr::safely(drive_download)
@@ -29,9 +35,10 @@ get_Drive_CPL_data <- function(g_id, data_name) {
   ## Run download function for all data files
   dl_return <- safe_drive_download(file = as_id(g_id), path = paste0(data_path, "/", data_name), overwrite = TRUE)
   
+  ## Log messages for success or failure
   if (is.null(dl_return$result)) {
     cat("\nSomething went wrong!\n")
-    dl_error <- as.character(dl_return$error)
+    dl_error <- as.character(dl_return$error) ## errors come back as lists sometimes so coerce to character
     cat("\n", dl_error, "\n")
   } else {
     res <- dl_return$result
@@ -41,6 +48,6 @@ get_Drive_CPL_data <- function(g_id, data_name) {
 
 ## Download all files from Google Drive!
 map2(data_csv$id, data_csv$name,
-     ~ get_Drive_CPL_data(g_id = .x, data_name = .y))
+     ~ get_drive_cpl_data(g_id = .x, data_name = .y))
 
 cat("\nAll done!\n")
